6f3c8d93178adabc434b3a973833f8b9d0474c3a
Hatim-Fangour <hatimfangour87@gmail.com>
2025-08-01 03:42:22 +0100

    added Base URl

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-
+console.log(process.env.REACT_APP_API_URL)
 // 1. Create configured Axios instance
 const api = axios.create({
   // baseURL: process.env.REACT_APP_API_URL || "http://localhost:8800/api",
     baseURL: process.env.NODE_ENV === 'production' 
     // ? 'https://yourdomain.com/api' 
-    ? 'http://localhost:8800/api' 
+    ? process.env.REACT_APP_API_URL 
     : "http://localhost:8800/api",
   timeout: 10000, // 10 seconds
   headers: {
